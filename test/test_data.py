c163f31b754e0b3644708c992afd895d8e5e0e0a
yuhymuk <yuhymuk@gmail.com>
2018-05-07 11:11:40 +0300

    added test file

---
+++ test/test_data.py
@@ -0,0 +1,24 @@
+import pytest
+import pytz
+from datetime import datetime
+
+from macd import MACD
+
+macd = MACD('btc_usd', 12, 26, 9, 60, '1')
+
+
+def convert_to_tmsp(date):
+    utc = pytz.timezone('UTC')
+    dt = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    return utc.localize(dt).timestamp()
+
+
+def test_skip_data():
+    from_ = '2018-05-01 00:00:00'
+    to_ = '2018-05-07 00:00:00'
+    data = macd.get_data(convert_to_tmsp(from_), convert_to_tmsp(to_))
+    assert data['minute_ts'][0] == from_
+
+
+def test_macs_coeff():
+    pass
